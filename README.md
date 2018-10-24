b3bb26c2d28ea13aff5f897bc08b3e6d8c98aa7a
Christophe <cavo789@yahoo.com>
2018-10-24 23:31:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# JSON Encode
+
+Very straight-forward script for encoding string in JSON.
+
+Format string like, f.i.,
+
+```
+Nuit d'Été, ç'eût
+```
+
+into
+
+```
+Nuit d'\u00c9t\u00e9, \u00e7'e\u00fbt
+```
+
+## How to use
+
+Just copy/paste your text into the text box as illustrated here below and click on the `Encode` button.
+
+![json_encode](images/json_encode.png)
---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * AUTHOR : AVONTURE Christophe
+ *
+ * Written date : 24 october 2018
+ *
+ * Simple JSON_Encode ajax interface.
+ */
+
+$task = filter_input(INPUT_POST, 'task', FILTER_SANITIZE_STRING);
+
+if ($task == 'encode') {
+    // Retrieve the JSON string
+    $JSON = base64_decode(filter_input(INPUT_POST, 'json', FILTER_SANITIZE_STRING));
+
+    // Return the encoded JSON
+    header('Content-Type: text/html');
+    $JSON = json_encode(utf8_encode($JSON));
+    echo base64_encode($JSON);
+
+    die();
+}
+
+// Sample string; with accentuated characters and
+$JSON = "this is a test\n".
+    "Paição São Paulo\n" .
+    "Nuit d'Été, ç'eût\n".
+    "(Sobre el Bé i el Mal) de Ciceró\n".
+    "inntrykk av å være lesbar";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="author" content="Christophe Avonture" />
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8;" />
+        <title>JSON Encode</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <style>
+            pre {
+                white-space: pre-wrap;       /* css-3 */
+                white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+                white-space: -pre-wrap;      /* Opera 4-6 */
+                white-space: -o-pre-wrap;    /* Opera 7 */
+                word-wrap: break-word;       /* Internet Explorer 5.5+ */
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="page-header"><h1>JSON Encode</h1></div>
+            <div class="container">
+                <div class="form-group">
+                    <label for="JSON">Copy/Paste your text in the 
+                        textbox below then click on the Encode button:</label>
+                    <textarea class="form-control" rows="5" id="JSON" name="JSON"><?php echo $JSON; ?></textarea>
+                </div>
+                <button type="button" id="btnEncode" class="btn btn-primary">Encode</button>
+                <hr/>
+                <pre id="Result"></pre>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+            $('#btnEncode').click(function(e)  {
+
+                e.stopImmediatePropagation();
+
+                var $data = new Object;
+                $data.task = "encode";
+                $data.json = window.btoa($('#JSON').val());
+
+                $.ajax({
+                    beforeSend: function() {
+                        $('#Result').html('<div><span class="ajax_loading">&nbsp;</span><span style="font-style:italic;font-size:1.5em;">Formatting...</span></div>');
+                        $('#btnEncode').prop("disabled", true);
+                    },
+                    async: true,
+                    type: "POST",
+                    url: "<?php echo basename(__FILE__); ?>",
+                    data: $data,
+                    datatype: "html",
+                    success: function (data) {
+                        $('#btnEncode').prop("disabled", false);
+                        $('#Result').html(window.atob(data));
+                    }
+                }); // $.ajax()
+            });
+        </script>
+    </body>
+</html>
