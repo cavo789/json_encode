cf1e37b263f81afe0539ed320d8e2cbb551dfa10
Christophe <cavo789@yahoo.com>
2018-12-31 17:15:53 +0100

    Abandonment of jQuery and migration to vue.js

--- index.php
+++ index.php
@@ -9,30 +9,28 @@
  * Written date : 24 october 2018
  *
  * Simple JSON_Encode ajax interface.
+ *
+ * Last mod:
+ * 2018-12-31 - Abandonment of jQuery and migration to vue.js
  */
 
 define('REPO', 'https://github.com/cavo789/json_encode');
 
-$task = filter_input(INPUT_POST, 'task', FILTER_SANITIZE_STRING);
+// Retrieve posted data
+$data = json_decode(file_get_contents('php://input'), true);
+if ($data !== []) {
+    $task = filter_var(($data['task'] ?? ''), FILTER_SANITIZE_STRING);
 
-if ('encode' == $task) {
-    // Retrieve the JSON string
-    $JSON = base64_decode(filter_input(INPUT_POST, 'json', FILTER_SANITIZE_STRING));
-
-    // Return the encoded JSON
-    header('Content-Type: text/html');
-    $JSON = json_encode(utf8_encode($JSON));
-    echo base64_encode($JSON);
-
-    die();
+    if ('encode' == $task) {
+        // Retrieve the JSON string
+        $JSON = base64_decode(filter_var(($data['json'] ?? ''), FILTER_SANITIZE_STRING));
+        // Return the encoded JSON
+        header('Content-Type: text/html');
+        $JSON = json_encode(utf8_encode($JSON));
+        echo base64_encode($JSON);
+        die();
+    }
 }
-
-// Sample string; with accentuated characters and
-$JSON = "this is a test\n" .
-    "Paição São Paulo\n" .
-    "Nuit d'Été, ç'eût\n" .
-    "(Sobre el Bé i el Mal) de Ciceró\n" .
-    'inntrykk av å være lesbar';
 
 // Get the GitHub corner
 $github = '';
@@ -66,9 +64,30 @@ if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
         <?php echo $github; ?>
         <div class="container">
             <div class="page-header"><h1>JSON Encode</h1></div>
-            <div class="container">
+            <div class="container" id="app">
                 <div class="form-group">
-                    <details>
+                    <how-to-use demo="https://raw.githubusercontent.com/cavo789/json_encode/master/images/demo.gif"></how-to-use>
+                    <label for="JSON">Copy/Paste your text in the 
+                        textbox below then click on the Encode button:</label>
+                    <textarea class="form-control" rows="5" v-model="JSON" name="JSON"></textarea>
+                </div>
+                <button type="button" @click="processEncode" class="btn btn-primary">Encode</button>
+                <hr/>
+                <pre v-html="HTML"></pre>
+            </div>
+        </div>
+        <script src="https://unpkg.com/vue"></script>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script type="text/javascript">
+            Vue.component('how-to-use', {
+                props: {
+                    demo: {
+                        type: String,
+                        required: true
+                    }
+                },
+                template:
+                    `<details>
                         <summary>How to use?</summary>
                         <div class="row">
                                 <div class="col-sm">
@@ -77,47 +96,30 @@ if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
                                         <li>Press the Encode button</li>
                                     </ul>
                                 </div>
-                                <div class="col-sm">
-                                    <img src="https://raw.githubusercontent.com/cavo789/json_encode/master/images/demo.gif" alt="Demo">
-                                </div>
+                                <div class="col-sm"><img v-bind:src="demo" alt="Demo"></div>
                             </div>
                         </div>
-                    </details>
-                    <label for="JSON">Copy/Paste your text in the 
-                        textbox below then click on the Encode button:</label>
-                    <textarea class="form-control" rows="5" id="JSON" name="JSON"><?php echo $JSON; ?></textarea>
-                </div>
-                <button type="button" id="btnEncode" class="btn btn-primary">Encode</button>
-                <hr/>
-                <pre id="Result"></pre>
-            </div>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-        <script type="text/javascript">
-            $('#btnEncode').click(function(e)  {
+                    </details>`
+            });
 
-                e.stopImmediatePropagation();
-
-                var $data = new Object;
-                $data.task = "encode";
-                $data.json = window.btoa($('#JSON').val());
-
-                $.ajax({
-                    beforeSend: function() {
-                        $('#Result').html('<div><span class="ajax_loading">&nbsp;</span><span style="font-style:italic;font-size:1.5em;">Formatting...</span></div>');
-                        $('#btnEncode').prop("disabled", true);
-                    },
-                    async: true,
-                    type: "POST",
-                    url: "<?php echo basename(__FILE__); ?>",
-                    data: $data,
-                    datatype: "html",
-                    success: function (data) {
-                        $('#btnEncode').prop("disabled", false);
-                        $('#Result').html(window.atob(data));
+            var app = new Vue({
+                el: '#app',
+                data: {
+                    JSON: "this is a test\nPaição São Paulo\nNuit d'Été, ç'eût\n" +
+                        "(Sobre el Bé i el Mal) de Ciceró\ninntrykk av å være lesbar",
+                    HTML: ''
+                },
+                methods: {
+                    processEncode() {
+                        var $data = {
+                            task: 'encode',
+                            json: window.btoa(this.JSON)
+                        }
+                        axios.post('<?php echo basename(__FILE__);?>', $data)
+                            .then(response => (this.HTML = window.atob(response.data)))
+                            .catch(function (error) {console.log(error);});
                     }
-                });
+                }
             });
         </script>
     </body>
